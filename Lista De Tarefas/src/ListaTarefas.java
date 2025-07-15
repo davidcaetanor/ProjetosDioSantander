15456e041eb1c02b13f2e69a1daa1278e1328f33
daka1 <davidcaetanoribeiro@gmail.com>
2025-07-14 22:28:57 -0300

    Inicializaçao do Set de Tarefas

---
+++ Lista De Tarefas/src/ListaTarefas.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaTarefas {
+    Set<Tarefa> tarefaSet;
+
+    public ListaTarefas() {
+        this.tarefaSet = new HashSet<>();
+    }
+
+    public void adicionarTarefa(String titulo, String desc) {
+        tarefaSet.add(new Tarefa(titulo, desc));
+    }
+
+}
---
+++ Lista De Tarefas/src/Tarefa.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class Tarefa {
+    private String tituloTarefa;
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(String tituloTarefa, String descricao) {
+        this.tituloTarefa = tituloTarefa;
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarefa tarefa = (Tarefa) o;
+        return Objects.equals(tituloTarefa, tarefa.tituloTarefa) && Objects.equals(descricao, tarefa.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tituloTarefa, descricao);
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    @Override
+    public String toString() {
+        return "Titulo da Tarefa: " + tituloTarefa + "\nDescricao da Tarefa: " + descricao + "\nConcluida: " + concluida;
+    }
+
+}
